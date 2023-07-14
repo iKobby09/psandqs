9762e506a92ac756e89058e8f8fb70da378d39bc
iKobby09 <77683806+iKobby09@users.noreply.github.com>
2023-07-14 01:54:13 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// Function to make elements draggable
+function makeDraggable(element) {
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    
+    // When the user presses the mouse button, initiate dragging
+    element.onmousedown = dragMouseDown;
+    
+    function dragMouseDown(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // Get the mouse cursor position at startup
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      // Call a function whenever the cursor moves
+      document.onmousemove = elementDrag;
+    }
+    
+    function elementDrag(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // Calculate the new cursor position
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      // Set the element's new position
+      element.style.top = (element.offsetTop - pos2) + "px";
+      element.style.left = (element.offsetLeft - pos1) + "px";
+    }
+    
+    function closeDragElement() {
+      // Stop dragging when the mouse button is released
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+  }
+  
+  // Get all draggable elements by class name and make them draggable
+  const draggableElements = document.getElementsByClassName("draggable");
+  for (let i = 0; i < draggableElements.length; i++) {
+    makeDraggable(draggableElements[i]);
+  }
+  
